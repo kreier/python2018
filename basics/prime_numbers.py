570125e8b0601a9920f97fc74f89d546d6ec9375
Matthias Kreier <43933271+matthiaskreier@users.noreply.github.com>
2018-11-16 14:03:12 +0700

    Add files via upload

---
+++ basics/prime_numbers.py
@@ -0,0 +1,191 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/hello_world.py 
+Hello world!
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/hello_world.py 
+Hello world!
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers 
+
+2, 3, 5, 
+Traceback (most recent call last):
+  File "//fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py", line 4, in <module>
+    if int(sqrt(i)) == sqrt(i):
+NameError: name 'sqrt' is not defined
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers 
+
+2, 3, 5, 
+11 , 
+13 , 
+15 , 
+17 , 
+19 , 
+21 , 
+23 , 
+25 , 
+27 , 
+29 , 
+31 , 
+33 , 
+35 , 
+37 , 
+39 , 
+41 , 
+43 , 
+45 , 
+47 , 
+49 , 
+51 , 
+53 , 
+55 , 
+57 , 
+59 , 
+61 , 
+63 , 
+65 , 
+67 , 
+69 , 
+71 , 
+73 , 
+75 , 
+77 , 
+79 , 
+81 , 
+83 , 
+85 , 
+87 , 
+89 , 
+91 , 
+93 , 
+95 , 
+97 , 
+99 , 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7,  11 ,  13 ,  15 ,  17 ,  19 ,  21 ,  23 ,  25 ,  27 ,  29 ,  31 ,  33 ,  35 ,  37 ,  39 ,  41 ,  43 ,  45 ,  47 ,  49 ,  51 ,  53 ,  55 ,  57 ,  59 ,  61 ,  63 ,  65 ,  67 ,  69 ,  71 ,  73 ,  75 ,  77 ,  79 ,  81 ,  83 ,  85 ,  87 ,  89 ,  91 ,  93 ,  95 ,  97 ,  99 ,  
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7,11 ,13 ,15 ,17 ,19 ,21 ,23 ,25 ,27 ,29 ,31 ,33 ,35 ,37 ,39 ,41 ,43 ,45 ,47 ,49 ,51 ,53 ,55 ,57 ,59 ,61 ,63 ,65 ,67 ,69 ,71 ,73 ,75 ,77 ,79 ,81 ,83 ,85 ,87 ,89 ,91 ,93 ,95 ,97 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,19 ,21 ,23 ,25 ,27 ,29 ,31 ,33 ,35 ,37 ,39 ,41 ,43 ,45 ,47 ,49 ,51 ,53 ,55 ,57 ,59 ,61 ,63 ,65 ,67 ,69 ,71 ,73 ,75 ,77 ,79 ,81 ,83 ,85 ,87 ,89 ,91 ,93 ,95 ,97 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 25 ,49 ,81 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,19 ,21 ,23 ,27 ,29 ,31 ,33 ,35 ,37 ,39 ,41 ,43 ,45 ,47 ,51 ,53 ,55 ,57 ,59 ,61 ,63 ,65 ,67 ,69 ,71 ,73 ,75 ,77 ,79 ,83 ,85 ,87 ,89 ,91 ,93 ,95 ,97 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,19 ,21 ,23 ,27 ,29 ,31 ,33 ,35 ,37 ,39 ,41 ,43 ,45 ,47 ,51 ,53 ,55 ,57 ,59 ,61 ,63 ,65 ,67 ,69 ,71 ,73 ,75 ,77 ,79 ,83 ,85 ,87 ,89 ,91 ,93 ,95 ,97 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 25 ,49 ,81 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,19 ,21 ,23 ,27 ,29 ,31 ,33 ,35 ,37 ,39 ,41 ,43 ,45 ,47 ,51 ,53 ,55 ,57 ,59 ,61 ,63 ,65 ,67 ,69 ,71 ,73 ,75 ,77 ,79 ,83 ,85 ,87 ,89 ,91 ,93 ,95 ,97 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11
+13
+15
+17
+19
+21
+23
+ 
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+ 
+51
+53
+55
+57
+59
+61
+63
+65
+67
+69
+71
+73
+75
+77
+79
+ 
+83
+85
+87
+89
+91
+93
+95
+97
+99
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, Traceback (most recent call last):
+  File "//fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py", line 6, in <module>
+    for x in range(2,math.sqrt(i),1):
+TypeError: 'float' object cannot be interpreted as an integer
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ' 13 ' 15 ' 17 ' 17 ' 19 ' 19 ' 21 ' 23 ' 23 ' 25 ' 25 ' 25 ' 27 ' 27 ' 29 ' 29 ' 29 ' 31 ' 31 ' 31 ' 33 ' 33 ' 35 ' 35 ' 35 ' 37 ' 37 ' 37 ' 37 ' 39 ' 39 ' 39 ' 41 ' 41 ' 41 ' 41 ' 43 ' 43 ' 43 ' 43 ' 45 ' 45 ' 47 ' 47 ' 47 ' 47 ' 49 ' 49 ' 49 ' 49 ' 49 ' 51 ' 51 ' 51 ' 51 ' 53 ' 53 ' 53 ' 53 ' 53 ' 55 ' 55 ' 55 ' 55 ' 57 ' 57 ' 57 ' 57 ' 59 ' 59 ' 59 ' 59 ' 59 ' 61 ' 61 ' 61 ' 61 ' 61 ' 63 ' 63 ' 63 ' 63 ' 65 ' 65 ' 65 ' 65 ' 65 ' 67 ' 67 ' 67 ' 67 ' 67 ' 67 ' 69 ' 69 ' 69 ' 69 ' 69 ' 71 ' 71 ' 71 ' 71 ' 71 ' 71 ' 73 ' 73 ' 73 ' 73 ' 73 ' 73 ' 75 ' 75 ' 75 ' 75 ' 77 ' 77 ' 77 ' 77 ' 77 ' 79 ' 79 ' 79 ' 79 ' 79 ' 79 ' 81 ' 81 ' 81 ' 81 ' 81 ' 81 ' 83 ' 83 ' 83 ' 83 ' 83 ' 83 ' 83 ' 85 ' 85 ' 85 ' 85 ' 85 ' 85 ' 87 ' 87 ' 87 ' 87 ' 87 ' 87 ' 89 ' 89 ' 89 ' 89 ' 89 ' 89 ' 89 ' 91 ' 91 ' 91 ' 91 ' 91 ' 91 ' 93 ' 93 ' 93 ' 93 ' 93 ' 93 ' 95 ' 95 ' 95 ' 95 ' 95 ' 95 ' 97 ' 97 ' 97 ' 97 ' 97 ' 97 ' 97 ' 99 ' 99 ' 99 ' 99 ' 99 ' 99 ' 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,17 ,19 ,19 ,21 ,23 ,23 ,25 ,25 ,25 ,27 ,27 ,29 ,29 ,29 ,31 ,31 ,31 ,33 ,33 ,35 ,35 ,35 ,37 ,37 ,37 ,37 ,39 ,39 ,39 ,41 ,41 ,41 ,41 ,43 ,43 ,43 ,43 ,45 ,45 ,47 ,47 ,47 ,47 ,49 ,49 ,49 ,49 ,49 ,51 ,51 ,51 ,51 ,53 ,53 ,53 ,53 ,53 ,55 ,55 ,55 ,55 ,57 ,57 ,57 ,57 ,59 ,59 ,59 ,59 ,59 ,61 ,61 ,61 ,61 ,61 ,63 ,63 ,63 ,63 ,65 ,65 ,65 ,65 ,65 ,67 ,67 ,67 ,67 ,67 ,67 ,69 ,69 ,69 ,69 ,69 ,71 ,71 ,71 ,71 ,71 ,71 ,73 ,73 ,73 ,73 ,73 ,73 ,75 ,75 ,75 ,75 ,77 ,77 ,77 ,77 ,77 ,79 ,79 ,79 ,79 ,79 ,79 ,81 ,81 ,81 ,81 ,81 ,81 ,83 ,83 ,83 ,83 ,83 ,83 ,83 ,85 ,85 ,85 ,85 ,85 ,85 ,87 ,87 ,87 ,87 ,87 ,87 ,89 ,89 ,89 ,89 ,89 ,89 ,89 ,91 ,91 ,91 ,91 ,91 ,91 ,93 ,93 ,93 ,93 ,93 ,93 ,95 ,95 ,95 ,95 ,95 ,95 ,97 ,97 ,97 ,97 ,97 ,97 ,97 ,99 ,99 ,99 ,99 ,99 ,99 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,15 ,17 ,19 ,23 ,25 ,29 ,31 ,35 ,37 ,41 ,43 ,47 ,49 ,53 ,59 ,61 ,67 ,71 ,73 ,79 ,83 ,89 ,97 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 ,13 ,17 ,19 ,23 ,29 ,31 ,37 ,41 ,43 ,47 ,53 ,59 ,61 ,67 ,71 ,73 ,79 ,83 ,89 ,97 ,
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 , 53 , 59 , 61 , 67 , 71 , 73 , 79 , 83 , 89 , 97 , 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 , 53 , 59 , 61 , 67 , 71 , 73 , 79 , 83 , 89 , 97 , 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7, , 11 , , 13 , , 17 , , 19 , , 23 , , 29 , , 31 , , 37 , , 41 , , 43 , , 47 , , 53 , , 59 , , 61 , , 67 , , 71 , , 73 , , 79 , , 83 , , 89 , , 97 , 
+>>> 
+ RESTART: //fs02-nb/AIS-Faculty/matthias.kreier/My Documents/Python/prime_numbers.py 
+Let's calculate prime numbers
+2, 3, 5, 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 , 53 , 59 , 61 , 67 , 71 , 73 , 79 , 83 , 89 , 97 
+>>> 
