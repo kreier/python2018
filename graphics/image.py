675dcee132e247b6a6c1fee99579747acaa57df8
SSIS students <43933271+kreier@users.noreply.github.com>
2022-10-13 16:11:21 +0700

    load and display image

---
+++ graphics/image.py
@@ -0,0 +1,14 @@
+# from PIL import Image  
+
+# width = 400
+# height = 300
+
+# img  = Image.new( mode = "RGB", size = (width, height) )
+# img.show()
+
+import pygame, time
+
+pygame.image.load('image.jpg')
+pygame.display.set_mode((900, 600))
+
+time.sleep(3)
---
+++ graphics/rectangle.py
@@ -0,0 +1,8 @@
+from PIL import Image  
+
+width = 400
+height = 300
+
+img  = Image.new( mode = "RGB", size = (width, height) )
+img.show()
+
